b26ad4122228b58179f97f27ede5a3d9037f2803
Lucas Antonio <lucas@arqlamp.com>
2025-09-25 10:46:40 -0300

    fix(charts): corrige integração de timezone e adiciona resiliência
    Resolve o conflito entre a gem 'groupdate' e o sistema de fuso horário por usuário do Redmine. A solução anterior, que forçava o timezone para UTC, foi revertida.
    
    - A nova abordagem passa o timezone do usuário atual ('Time.zone') diretamente para o helper do gráfico, garantindo uma integração perfeita com o Redmine.
    - A renderização dos gráficos agora é envolvida em um bloco 'begin/rescue' para tratar exceções e exibir mensagens de erro amigáveis.
    - O controller foi refatorado para carregar o helper do Chartkick de forma mais segura.
    - Remove arquivos de configuração e hooks desnecessários que poderiam causar conflitos.

--- config/application.rb
+++
@@ -1,9 +0,0 @@
-module RedmineApp
-  class Application < Rails::Application
-    # ...existing code...
-
-    config.eager_load_paths << Rails.root.join('plugins', 'foton_contacts', 'lib')
-
-    # ...existing code...
-  end
-end
--- lib/foton_contacts/hooks.rb
+++
@@ -1,9 +0,0 @@
-# lib/foton_contacts/hooks.rb
-module FotonContacts
-  class Hooks < Redmine::Hook::ViewListener
-    def view_layouts_base_html_head(context)
-      stylesheet_link_tag('contacts', plugin: 'foton_contacts') +
-      javascript_include_tag('contacts', plugin: 'foton_contacts')
-    end
-  end
-end
