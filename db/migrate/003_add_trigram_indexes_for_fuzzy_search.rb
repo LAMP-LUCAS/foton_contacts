be7dc80dde7ac1fe37cbc7d6f25b0f54b4404156
Lucas Antonio <lucas@arqlamp.com>
2025-10-20 17:01:27 -0300

    refactor(db): consolida migrações em único arquivo de schema inicial
    Unifica todas migrações de banco de dados (001 a 005) em um arquivo,

--- db/migrate/003_add_trigram_indexes_for_fuzzy_search.rb
+++
@@ -1,8 +0,0 @@
-class AddTrigramIndexesForFuzzySearch < ActiveRecord::Migration[7.1]
-  def change
-    add_index :foton_contacts, :name, using: :gin, opclass: { name: :gin_trgm_ops } unless index_exists?(:foton_contacts, :name, using: :gin)
-    add_index :foton_contact_addresses, :address, using: :gin, opclass: { address: :gin_trgm_ops } unless index_exists?(:foton_contact_addresses, :address, using: :gin)
-    add_index :foton_contact_emails, :email unless index_exists?(:foton_contact_emails, :email)
-    add_index :foton_contact_phones, :phone unless index_exists?(:foton_contact_phones, :phone)
-  end
-end
